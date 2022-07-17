d432d3681f725b43baf51b16b61ce7cf2f5001e4
JsomeDev <109468380+JsomeDev@users.noreply.github.com>
2022-07-18 03:48:04 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+const images = ["img.avif", "img2.avif", "img6.avif","img8.webp","img9.webp"];
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+
+bgImage.src = `img/${chosenImage}`;
+
+bgImage.classList.add("bgImg");
+
+document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(${bgImage.src})`; 
